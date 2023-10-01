89ebb4bd58bef390d8a67b9a18749106fe647757
atosaramis <78604182+atosaramis@users.noreply.github.com>
2023-10-01 19:54:13 -0400

    Update brave.py

--- brave.py
+++ brave.py
@@ -1,40 +1,54 @@
+# app.py
+
 import streamlit as st
 import requests
 
 BRAVE_API_ENDPOINT = "https://api.search.brave.com/news"
+API_KEY = "YOUR_BRAVE_API_KEY"  # Replace with your actual Brave API key
 
-def brave_search(api_key, query):
+def brave_search(query):
     headers = {
-        "x-rapidapi-key": api_key,
+        "x-rapidapi-key": API_KEY,
         "x-rapidapi-host": "api.search.brave.com"
     }
     params = {"q": query}
     response = requests.get(BRAVE_API_ENDPOINT, headers=headers, params=params)
-    
-    if response.status_code != 200:
-        st.error(f"Error {response.status_code}: {response.text}")
-        return None
-
-    try:
-        return response.json()
-    except requests.exceptions.JSONDecodeError:
-        st.error("Failed to decode the response from the server. The response might not be in JSON format.")
-        st.text(response.text)
-        return None
+    return response.json()
 
 st.title("Brave Search Interface")
 
-# Input for API Key
-api_key = st.text_input("Enter your Brave API key:", type="password")
-
-# Input for Search Query
+# Search Bar
 search_query = st.text_input("Enter your search query:")
 
-if st.button("Search") and api_key:
-    results = brave_search(api_key, search_query)
-    if results and 'data' in results:
+# Search Button
+if st.button("Search"):
+    results = brave_search(search_query)
+    if 'data' in results:
         for article in results['data']:
-            st.markdown(f"### [{article['title']}]({article['url']})")
-            st.markdown(article['description'])
+            st.subheader(article['title'])
+            st.write(article['description'])
+            st.write(article['url'])
             st.write("---")
+    else:
+        st.error("Error fetching results. Please try again.")
 
+# Pagination (if the API supports it)
+page = st.slider("Page", 1, 10)
+if page > 1:
+    results = brave_search(search_query, page)
+    for article in results['data']:
+        st.subheader(article['title'])
+        st.write(article['description'])
+        st.write(article['url'])
+        st.write("---")
+
+# Error Handling
+try:
+    results = brave_search(search_query)
+except requests.exceptions.RequestException as e:
+    st.error(f"API Error: {e}")
+
+# API Key Management
+api_key_input = st.text_input("Enter your API key:", type="password")
+if api_key_input:
+    API_KEY = api_key_input
